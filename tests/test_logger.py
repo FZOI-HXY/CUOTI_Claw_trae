326455fbc279d979f62c2e4431971032547f685f
DESKTOP-75F7HDL\IDC <huxiaoyang1234@outlook.com>
2026-07-04 21:06:41 +0800

    test: 删除 test_logger.py 中零断言和测试标准库行为的无价值测试

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -26,20 +26,6 @@ class TestSetupLogger:
         logger = setup_logger("TestLogger2")
         assert len(logger.handlers) > 0
 
-    def test_same_name_reuses_logger(self):
-        from logger import setup_logger
-        logger_a = setup_logger("TestLogger_Dup")
-        count_a = len(logger_a.handlers)
-        logger_b = setup_logger("TestLogger_Dup")
-        assert len(logger_b.handlers) == count_a
-        assert logger_a is logger_b
-
-    def test_different_names_different_loggers(self):
-        from logger import setup_logger
-        l1 = setup_logger("Logger_A")
-        l2 = setup_logger("Logger_B")
-        assert l1 is not l2
-
 
 @pytest.mark.unit
 class TestLoggerHandlers:
@@ -51,29 +37,6 @@ class TestLoggerHandlers:
             for h in logger.handlers
         )
         assert has_console
-
-
-@pytest.mark.unit
-class TestLoggerWriting:
-    def test_info_log(self):
-        from logger import setup_logger
-        logger = setup_logger("TestLogger_W1")
-        logger.info("Test info")
-
-    def test_warning_log(self):
-        from logger import setup_logger
-        logger = setup_logger("TestLogger_W2")
-        logger.warning("Test warning")
-
-    def test_error_log(self):
-        from logger import setup_logger
-        logger = setup_logger("TestLogger_W3")
-        logger.error("Test error")
-
-    def test_format_log(self):
-        from logger import setup_logger
-        logger = setup_logger("TestLogger_W4")
-        logger.info("User %s uploaded %s", "admin", "test.jpg")
 
 
 @pytest.mark.unit
